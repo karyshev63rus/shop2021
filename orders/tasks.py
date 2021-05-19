16ad8e5e1a4fbad145be923ebe3ddfc3b36af908
karyshev_m <karyshev63rus@gmail.com>
2021-05-19 12:31:09 +0400

    translation feature is realised

--- orders/tasks.py
+++ orders/tasks.py
@@ -8,5 +8,5 @@ def order_created(order_id):
     order = Order.objects.get(id=order_id)
     subject = f'Order nr. {order.id}'
     message = f'Dear {order.first_name}, \n\nYou have successfilly placed an order. Your order id is {order.id}.'
-    mail_sent = send_mail(subject, message, 'admin@shop2021.com', [order.email])
+    mail_sent = send_mail(subject, message, 'mike6376@yandex.ru', [order.email])
     return mail_sent
